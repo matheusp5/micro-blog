51a41af76f3b863392d81f9317aee71d70e481e0
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-27 11:20:34 -0300

    feat(core): created date to created at

---
+++ MicroBlog.Posts/Migrations/20230827141903_CreatedAt.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroBlog.Posts.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Posts",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Categories",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Posts",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Categories",
+                newName: "CreatedDate");
+        }
+    }
+}
