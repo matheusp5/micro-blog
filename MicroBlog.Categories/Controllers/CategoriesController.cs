7848f624121a3ccbf3eaefc0a5c8d08005002a53
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-27 11:11:34 -0300

    feat(categories): create categories project

---
+++ MicroBlog.Categories/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroBlog.Categories.Controllers;
+
+[ApiController]
+[Route("api/categories")]
+public class CategoriesController : ControllerBase
+{
+    
+}
--- MicroBlog.Core/Infrastructure/DataContext.cs
+++ MicroBlog.Core/Infrastructure/DataContext.cs
@@ -11,4 +11,8 @@ public class DataContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Post> Posts  { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
 }
