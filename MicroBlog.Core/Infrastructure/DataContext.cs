716cf32c824f16ee29ad8033fabfddd7adc5fee3
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-27 10:32:51 -0300

    feat(core): created data context

---
+++ MicroBlog.Core/Infrastructure/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using MicroBlog.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroBlog.Core.Infrastructure;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts  { get; set; }
+}
