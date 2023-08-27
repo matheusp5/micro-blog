3ee2fe992b72c249c5683452eb02ff16f8b19086
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-27 11:17:55 -0300

    feat(categories): categories controller seed

--- MicroBlog.Categories/Controllers/CategoriesController.cs
+++ MicroBlog.Categories/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
+using MicroBlog.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroBlog.Categories.Controllers;
 
@@ -6,5 +8,27 @@ namespace MicroBlog.Categories.Controllers;
 [Route("api/categories")]
 public class CategoriesController : ControllerBase
 {
+    private readonly DataContext _dataContext;
+
+    public CategoriesController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await this._dataContext
+            .Categories
+            .ToListAsync());
+    }
     
+    
+    [HttpGet("category/{id}")]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        return Ok(await this._dataContext
+            .Categories
+            .FirstOrDefaultAsync(c => c.Id == id));
+    }
 }
